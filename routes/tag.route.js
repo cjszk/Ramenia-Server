f0677157a34f63aabee29c7d95ca1725331cc3a6
Christopher <cjszk@hotmail.com>
2018-09-15 11:58:03 -0700

    more admin endpoints

--- routes/tag.route.js
+++ routes/tag.route.js
@@ -6,28 +6,57 @@ const mongoose = require('mongoose');
 const Tag = require('../models/tag');
 const Ramen = require('../models/ramen');
 
-router.post('/post-tag/:id', (req, res, next) => {
+router.get('/tags', (req, res, next) => {
+    Tag.find()
+        .then((result) => {
+            res.json(result);
+        })
+        .catch((err) => {
+            next(err);
+        })
+})
 
-    const {id} = req.params;
+router.post('/tags', (req, res, next) => {
 
     const {name} = req.body;
 
     Tag.create({name})
         .then((result) => {
-            const tagId = result.id;
-            Ramen.findById(id)
-                .then((result) => {
-                    let ramen = result;
-                    ramen.tags.push(tagId)
-                    Ramen.findByIdAndUpdate(id, ramen)
-                        .then((result) => res.json(result))
-                        .catch((err) => next(err))
-                })
-                .catch((err) => next(err));
+            res.json(result);
         })
         .catch((err) => next(err));
 
 })
 
+router.put('/tags-ramen', (req, res, next) => {
+
+    const { tagId, ramenId } = req.body;
+
+    Ramen.findById(ramenId)
+        .then((result) => {
+            let ramen = result;
+            ramen.tags.push(tagId);
+            Ramen.findByIdAndUpdate(ramenId, ramen)
+                .then((result) => res.json(result))
+                .catch((err) => next(err));
+        })
+        .catch((err) => next(err))
+})
+
+router.put('/remove-tags-ramen', (req, res, next) => {
+
+    const { tagId, ramenId } = req.body;
+
+    Ramen.findById(ramenId)
+        .then((result) => {
+            let ramen = result;
+            let tags = result.tags.filter((tag) => String(tag) !== String(tagId));
+            ramen.tags = tags;
+            Ramen.findByIdAndUpdate(ramenId, ramen)
+                .then((result) => res.json(result))
+                .catch((err) => next(err));
+        })
+        .catch((err) => next(err))
+})
 
 module.exports = router;
